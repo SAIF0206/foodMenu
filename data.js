95100276c91704db884aaf91bc563eb1d0136e0f
SAIF0206 <msaif.sam@gmail.com>
2024-01-14 18:04:37 +0800

    Updates

---
+++ data.js
@@ -0,0 +1,32 @@
+const FoodItem = [
+  {
+    name: "Carrot Halwa",
+    ingredients: "Carrots, Sugar, Condense Milk",
+    photoName: "items/carrotHalwa.jpeg",
+  },
+  {
+    name: "Mutton Paya",
+    ingredients: "Mutton Legs",
+    photoName: "items/mutton.jpeg",
+  },
+  {
+    name: "Chicken Lolipop",
+    ingredients: "Deep fried chicken wings",
+    photoName: "items/chicken.jpeg",
+  },
+  {
+    name: "Pompfret Fish",
+    ingredients: "White Pompfret Fish tawa fry",
+    photoName: "items/fish.jpeg",
+  },
+  {
+    name: "Paneer Pratha",
+    ingredients: "Indian cottage cheeze stuffed pratha",
+    photoName: "items/paneer.jpeg",
+  },
+  {
+    name: "Jalebi",
+    ingredients: "Sweet dish tu banti hai",
+    photoName: "items/Jalebi.jpeg",
+  },
+];
